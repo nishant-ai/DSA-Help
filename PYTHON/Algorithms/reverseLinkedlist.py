a68ec37f2f9856e10337ddcda1d67c8e9f43926c
Ahmed Eltaher <ahmedheltaher@gmail.com>
2022-10-09 16:09:28 +0200

    Added Reverse for linked list

---
+++ PYTHON/Algorithms/reverseLinkedlist.py
@@ -0,0 +1,26 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def printList(self):
+        temp = self.head
+        while(temp):
+            print(temp.data)
+            temp = temp.next
+
+    def reverse(self):
+        prev = None
+        current = self.head
+        while(current is not None):
+            next = current.next
+            current.next = prev
+            prev = current
+            current = next
+        self.head = prev
+
+# this code is contributed by @ahmedheltaher
