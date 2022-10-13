84c909250c94e6e76e19a19b243073c5e97e3bbe
Pavan <60210984+pavanpakhare@users.noreply.github.com>
2022-10-13 12:36:33 +0530

    Quick sort file added

---
+++ PYTHON/Algorithms/Sorting/Quick-Sort.py
@@ -0,0 +1,50 @@
+#Quick sort 
+
+
+def partition(array, low, high):
+
+  
+  pivot = array[high]
+
+  
+  i = low - 1
+
+ 
+  for j in range(low, high):
+    if array[j] <= pivot:
+   
+      i = i + 1
+
+      
+      (array[i], array[j]) = (array[j], array[i])
+
+  
+  (array[i + 1], array[high]) = (array[high], array[i + 1])
+
+  
+  return i + 1
+
+
+def quickSort(array, low, high):
+  if low < high:
+
+
+    pi = partition(array, low, high)
+
+    
+    quickSort(array, low, pi - 1)
+
+    
+    quickSort(array, pi + 1, high)
+
+
+data = [8, 7, 2, 1, 0, 9, 6]
+print("Unsorted Array")
+print(data)
+
+size = len(data)
+
+quickSort(data, 0, size - 1)
+
+print('Sorted Array in Ascending Order:')
+print(data)
